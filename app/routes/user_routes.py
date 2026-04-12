631f227ce67bf29829a30e1d4e68f2717530481b
Mikelle <mmikelle.wade@adadevelopersacademy.org>
2026-04-12 17:22:48 -0400

    implements polling consumer for SQS queue and re-structures for service layer

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,7 +1,7 @@
 from ..db import db
 from ..models.user import User
-from flask import Blueprint, request, Response
-from .route_utilities import create_model, get_models_with_filters, validate_model, update_model, get_user_by_email
+from flask import Blueprint, request, Response, abort, make_response
+from ..utilities import create_model, get_models_with_filters, validate_model, update_model, get_user_by_email
 
 bp = Blueprint("users_blueprint", __name__, url_prefix="/users")
 
@@ -10,7 +10,14 @@ bp = Blueprint("users_blueprint", __name__, url_prefix="/users")
 def create_user():
     request_body = request.get_json()
 
-    return create_model(User, request_body)
+    try:
+        new_user = create_model(User, request_body)
+    except KeyError as e:
+        abort(make_response({"message": f"Invalid: Missing key ({e.args[0]})"}, 400))
+    except Exception:
+        abort(make_response({"message": f"An account with email ({request_body['email']}) already exists."}, 409))
+
+    return new_user.to_dict(), 201
 
 
 @bp.get("/")
@@ -20,7 +27,12 @@ def get_all_users():
 
 @bp.get("/<id>")
 def get_single_user(id):
-    user = validate_model(User, id)
+    try:
+        user = validate_model(User, id)
+    except ValueError as e:
+        abort(make_response({"message": str(e)}, 400))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
 
     return user.to_dict()
 
@@ -29,20 +41,39 @@ def get_single_user(id):
 def get_single_user_by_email():
     email = request.args.get("email")
 
-    return get_user_by_email(email)
+    try:
+        user = get_user_by_email(email)
+    except RuntimeError as e:
+        abort(make_response({"message": str(e)}, 500))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
+
+    return user.to_dict()
 
 
 @bp.put("/<id>")
 def update_user(id):
-    user = validate_model(User, id)
-    request_body = request.get_json()
+    try:
+        user = validate_model(User, id)
+    except ValueError as e:
+        abort(make_response({"message": str(e)}, 400))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
 
-    return update_model(user, request_body)
+    request_body = request.get_json()
+    update_model(user, request_body)
+
+    return Response(status=204, mimetype="application/json")
 
 
 @bp.delete("/<id>")
 def delete_user(id):
-    user = validate_model(User, id)
+    try:
+        user = validate_model(User, id)
+    except ValueError as e:
+        abort(make_response({"message": str(e)}, 400))
+    except LookupError as e:
+        abort(make_response({"message": str(e)}, 404))
 
     db.session.delete(user)
     db.session.commit()
