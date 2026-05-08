090a409c85efa49d29177b32fed2722a8b2af659
apradoada <adrian.prado@adadevelopersacademy.org>
2026-05-08 10:29:07 -0500

    Added Docker file for deployment and modified environment variables to better meet AWS needs

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,39 @@
 from flask import Flask
 from flask_cors import CORS
+from sqlalchemy.engine import URL
 from .db import db, migrate
 from .models.user import User
 from .routes.user_routes import bp as users_bp 
 
 import os
+import boto3
+import json
+
 
 def create_app(config=None):
     app = Flask(__name__)
     CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    DB_HOST = os.environ.get('DATABASE_HOST')
+    DB_PORT = int(os.environ.get('DATABASE_PORT', 5432))
+    DB_NAME = os.environ.get('DATABASE_NAME')
+    DB_USER = os.environ.get('DATABASE_USER')
+
+
+    secrets_client = boto3.client('secretsmanager')
+    secret = secrets_client.get_secret_value(SecretId=os.environ.get('DB_SECRET_ARN'))
+    DB_PASSWORD = json.loads(secret['SecretString'])['password']
+
+    connection_url = URL.create(
+        drivername='postgresql+psycopg2',
+        username=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME
+    )
+    app.config['SQLALCHEMY_DATABASE_URI'] = connection_url
 
     if config:
         app.config.update(config)
@@ -24,7 +46,13 @@ def create_app(config=None):
     @app.get('/')
     def index():
         return {
-        "status": "ok"
+            "status": "ok"
+        }
+    
+    @app.get('/health')
+    def health():
+        return {
+        "status": "healthy"
         }
 
     return app
